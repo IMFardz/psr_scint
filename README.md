72ae5238b10bcea476c57d90d2ed44071a3992e8
Dana Simard <simard@cita.utoronto.ca>
2018-04-06 11:13:10 -0400

    Initial layout for package

---
+++ README.md
@@ -0,0 +1,4 @@
+## Package PyScint
+A python package for analyzing pulsar dynamic spectra.
+
+Requires astropy, numpy
---
+++ pyscint/__init__.py
@@ -0,0 +1,11 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+#
+# A package for handling and analyzing pulsar dynamic spectra
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+if not _ASTROPY_SETUP_:
+    # For egg_info test builds to pass, put package imports here.
+    pass
+    #from .example_mod import *
